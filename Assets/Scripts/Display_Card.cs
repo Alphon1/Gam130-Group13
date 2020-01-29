56b84c90db7173c3b8f120fe1b30661a0dfe5855
Alex Williamson <alex_williamson@rocketmail.com>
2020-01-29 17:47:31 +0000

    Added a script to display a card on the blockout, for testing purposes.

---
+++ Assets/Scripts/Display_Card.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Display_Card : MonoBehaviour
+{
+    public Card Card;
+    public Text Name;
+    public Text Cost;
+    public Text Description;
+    public Image Art;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Name.text = Card.Name;
+        Cost.text = Card.Cost.ToString();
+        Description.text = Card.Description;
+        Art.sprite = Card.Art;
+    }
+
+}
