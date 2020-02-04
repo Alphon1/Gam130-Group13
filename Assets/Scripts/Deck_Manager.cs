b991a2d2a7a3573879feaff4889ccd59e9fdf384
Alphon1 <alex_williamson@rocketmail.com>
2020-02-04 17:21:34 +0000

    Fixed the Shuffle_Deck function

--- Assets/Scripts/Deck_Manager.cs
+++ Assets/Scripts/Deck_Manager.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class Deck_Manager : MonoBehaviour
 {
     [SerializeField]
-    public List<Card> Deck;
-    private List<Card> Temp_Deck;
+    private List<Card> Deck;
     private List<Card> Discard;
     private int Random_Position;
+    private Card Temp_Card;
 
     public void Shuffle_Deck()
     {
         for (int i = 0; i < Deck.Count; i++)
         {
-            Random_Position = Random.Range(0, Deck.Count);
-            Temp_Deck[i] = Deck[Random_Position];
-            Deck.RemoveAt(Random_Position);
+            Temp_Card = Deck[i];
+            Random_Position = Random.Range(i, Deck.Count);
+            Deck[i] = Deck[Random_Position];
+            Deck[Random_Position] = Temp_Card;
         }
-        Deck = Temp_Deck;
     }
 
     public void Deck_Out()
