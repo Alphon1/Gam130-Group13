a966c11af3c12ed3eeb26a10a62b3cff349fcb7e
Tosh-Jompson <giblets2k@gmail.com>
2020-02-13 17:03:42 +0000

    Added PCG scripts and placeholders

---
+++ Assets/Scripts/BasicRoomScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BasicRoomScript : MonoBehaviour {
+
+    public Doorways[] doorways;
+    public BoxCollider boxCollider;
+
+    public Bounds RoomBounds
+    {
+        get { return boxCollider.bounds; }
+    }
+
+}
