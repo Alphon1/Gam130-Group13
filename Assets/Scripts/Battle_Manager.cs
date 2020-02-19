9f14a4bf99b2569954d7a6f334eddeaf1ffdeab1
Alex Williamson <alex_williamson@rocketmail.com>
2020-02-19 14:22:07 +0000

    Added a Battle_Manager script that manages turn changes between the Player and Enemies.

---
+++ Assets/Scripts/Battle_Manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle_Manager : MonoBehaviour
+{
+    public bool Is_Player_Turn;
+    private GameObject Player;
+
+    void Start()
+    {
+        Is_Player_Turn = true;
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    public void Turn_Switch()
+    {
+        if (Is_Player_Turn == true)
+        {
+            Is_Player_Turn = false;
+            for (int i = 0; i < GameObject.FindGameObjectsWithTag("Enemy").Length; i++)
+            {
+                GameObject.FindGameObjectsWithTag("Enemy")[i].GetComponent<Enemy_Manager>().Enemy_Turn();
+            }
+            Turn_Switch();
+            Player.GetComponent<Hand_Manager>().Reset_Hand();
+        }
+        else
+        {
+            Is_Player_Turn = true;
+        }
+    }
+
+    public void End_Turn()
+    {
+        if (Is_Player_Turn)
+        {
+            Turn_Switch();
+        }
+    }
+}
--- Assets/Scripts/Hand_Manager.cs
+++ Assets/Scripts/Hand_Manager.cs
@@ -8,10 +8,12 @@ public class Hand_Manager : Deck_Manager
     private List<Card> Hand;
     private GameObject Target;
     private GameObject Player;
+    private GameObject Turn_Order;
 
     private void Awake()
     {
         Player = GameObject.FindWithTag("Player");
+        Turn_Order = GameObject.FindWithTag("Battle_Manager");
     }
 
     public void Draw_Card(int Cards_Drawn)
@@ -31,76 +33,79 @@ public class Hand_Manager : Deck_Manager
     }
     public void Play_Card(Card Played_Card)
     {
-        if (Player.GetComponent<Player_Manager>().Can_Play(Played_Card.Cost))
+        if (Turn_Order.GetComponent<Battle_Manager>().Is_Player_Turn == true)
         {
-            for (int i = 0; i < Played_Card.Functions.Count; i++)
+            if (Player.GetComponent<Player_Manager>().Can_Play(Played_Card.Cost))
             {
-                switch (Played_Card.Functions[i])
+                for (int i = 0; i < Played_Card.Functions.Count; i++)
                 {
-                    case "Draw":
-                        Draw_Card(Played_Card.Function_Values[i]);
-                        break;
-                    case "Damage":
-                        Target_Select();
-                        if (Target.tag == "Enemy")
-                        {
-                            Target.GetComponent<Enemy_Manager>().Damage(Played_Card.Function_Values[i]);
+                    switch (Played_Card.Functions[i])
+                    {
+                        case "Draw":
+                            Draw_Card(Played_Card.Function_Values[i]);
                             break;
-                        }
-                        else
-                        {
-                            goto Not_Played;
-                        }
-                    case "Heal":
-                        Player.GetComponent<Player_Manager>().Health_Change(-(Played_Card.Function_Values[i]));
-                        break;
-                    case "Add Energy":
-                        Player.GetComponent<Player_Manager>().Energy_Change(-(Played_Card.Function_Values[i]));
-                        break;
-                    case "Lethal Damage":
-                        Target_Select();
-                        if (Target.tag == "Enemy")
-                        {
-                            if (Target.GetComponent<Enemy_Manager>().Lethal_Damage(Played_Card.Function_Values[i]) == false)
+                        case "Damage":
+                            Target_Select();
+                            if (Target.tag == "Enemy")
                             {
-                                goto End_Card;
+                                Target.GetComponent<Enemy_Manager>().Damage(Played_Card.Function_Values[i]);
+                                break;
+                            }
+                            else
+                            {
+                                goto Not_Played;
+                            }
+                        case "Heal":
+                            Player.GetComponent<Player_Manager>().Health_Change(-(Played_Card.Function_Values[i]));
+                            break;
+                        case "Add Energy":
+                            Player.GetComponent<Player_Manager>().Energy_Change(-(Played_Card.Function_Values[i]));
+                            break;
+                        case "Lethal Damage":
+                            Target_Select();
+                            if (Target.tag == "Enemy")
+                            {
+                                if (Target.GetComponent<Enemy_Manager>().Lethal_Damage(Played_Card.Function_Values[i]) == false)
+                                {
+                                    goto End_Card;
+                                }
+                                break;
+                            }
+                            else
+                            {
+                                goto Not_Played;
+                            }
+                        case "Shuffle Discard":
+                            for (int j = 0; j < Discard.Count; j++)
+                            {
+                                Deck.Add(Discard[j]);
+                                Discard.RemoveAt(j);
+                            }
+                            Shuffle_Deck();
+                            break;
+                        case "Random Damage":
+                            for (int j = 0; j < Played_Card.Function_Values[i]; j++)
+                            {
+
+                                GameObject.FindGameObjectsWithTag("Enemy")[Random.Range(0, GameObject.FindGameObjectsWithTag("Enemy").Length)].GetComponent<Enemy_Manager>().Damage(1);
                             }
                             break;
-                        }
-                        else
-                        {
-                            goto Not_Played;
-                        }
-                    case "Shuffle Discard":
-                        for (int j = 0; j < Discard.Count; j++)
-                        {
-                            Deck.Add(Discard[j]);
-                            Discard.RemoveAt(j);
-                        }
-                        Shuffle_Deck();
-                        break;
-                    case "Random Damage":
-                        for(int j = 0; j < Played_Card.Function_Values[i]; j++)
-                        {
-
-                            GameObject.FindGameObjectsWithTag("Enemy")[Random.Range(0, GameObject.FindGameObjectsWithTag("Enemy").Length)].GetComponent<Enemy_Manager>().Damage(1);
-                        }
-                        break;
-                    case "AOE Damage":
-                        for (int j = 0; j < GameObject.FindGameObjectsWithTag("Enemy").Length; j++)
-                        {
-                            GameObject.FindGameObjectsWithTag("Enemy")[j].GetComponent<Enemy_Manager>().Damage(Played_Card.Function_Values[i]);
-                        }
+                        case "AOE Damage":
+                            for (int j = 0; j < GameObject.FindGameObjectsWithTag("Enemy").Length; j++)
+                            {
+                                GameObject.FindGameObjectsWithTag("Enemy")[j].GetComponent<Enemy_Manager>().Damage(Played_Card.Function_Values[i]);
+                            }
                             break;
-                    case null:
-                        break;
+                        case null:
+                            break;
+                    }
                 }
+                End_Card:;
+                Hand.Remove(Played_Card);
+                Discard.Add(Played_Card);
+                Player.GetComponent<Player_Manager>().Energy_Change(Played_Card.Cost);
+                Not_Played:;
             }
-            End_Card:;
-            Hand.Remove(Played_Card);
-            Discard.Add(Played_Card);
-            Player.GetComponent<Player_Manager>().Energy_Change(Played_Card.Cost);
-            Not_Played:;
         }
     }
 
