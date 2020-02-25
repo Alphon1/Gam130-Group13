892b60bc116831892ae2b12178aa40f15688685e
Alphon1 <alex_williamson@rocketmail.com>
2020-02-25 16:54:37 +0000

    Added a function to update the cards displayed in the battle UI

---
+++ Assets/Scripts/Card_Game/Card_Display.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card_Display : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Display_Card(Card Displayed_Card)
+    {
+        
+    }
+}
