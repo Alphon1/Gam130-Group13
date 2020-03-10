84ac47998444a0d2731ba3172840eb257288d368
J-Dogg <giblets2k@gmail.com>
2020-03-10 14:49:47 +0000

    Battle UI activates on move

--- Assets/Scripts/PCG_SCR/SCR_BRMove.cs
+++ Assets/Scripts/PCG_SCR/SCR_BRMove.cs
@@ -9,12 +9,15 @@ public class SCR_BRMove : MonoBehaviour
     public float offsetX;
     public float offsetY;
     public float offsetZ;
+    public GameObject battleUI;
     public void Move()
     {
         player = GameObject.Find("Player");
         room = GameObject.FindGameObjectWithTag("BossRoom");
+        battleUI = GameObject.Find("Temp_Battle_UI");
         Debug.Log("Player Move");
         player.transform.position = room.transform.position;
         player.transform.position = player.transform.position + new Vector3(offsetX, offsetY, offsetZ);
+        battleUI.SetActive(true);
     }
 }
