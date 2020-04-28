5ee5e0af5d1999abf796a05d57739b9cc52272e3
giblets2k <giblets2k@gmail.com>
2020-04-28 15:28:18 +0100

    Added post tutorial dialogue trigger

---
+++ Assets/Scripts/RU3_fight_detect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RU3_fight_detect : MonoBehaviour
+{
+    public GameObject RU3;
+    public GameObject PostDialogue;
+    public bool fightOver = false;
+    private Enemy_Manager Enemyscript;
+
+    void Update()
+    {
+        if (Enemyscript.Health >= 0)
+        {
+            //fightOver = true;
+            PostDialogue.SetActive(true);
+        }
+    }
+}
