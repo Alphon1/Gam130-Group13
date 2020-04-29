efa24f180601b592364e82056e78509aa89d4303
giblets2k <giblets2k@gmail.com>
2020-04-29 17:02:02 +0100

    Fixed PCG combat and HealthRoom.prefab now heals

---
+++ Assets/Scripts/RoomHeal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomHeal : MonoBehaviour
+{
+    private Player_Manager playerScript;
+    public GameObject Player;
+    public GameObject battleUI;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player_UI");
+        playerScript = Player.GetComponent<Player_Manager>();
+        battleUI = GameObject.Find("Battle_UI_V3");
+    }
+
+    private void OnCollisionEnter(Collision Player)
+    {
+        Debug.Log("Enter");
+        battleUI.SetActive(true);
+        playerScript.Health = 50;
+        battleUI.SetActive(false);
+    }
+}
